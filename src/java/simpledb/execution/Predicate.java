c88e82cdbb14506850dd19a9fd9a0dfaa6c17f85
chengr25 <384372602@qq.com>
2022-04-25 10:59:09 +0800

    lab2 通过PredicateTest

--- src/java/simpledb/execution/Predicate.java
+++ src/java/simpledb/execution/Predicate.java
@@ -12,6 +12,12 @@ public class Predicate implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    private int fieldNo;
+
+    private Op op;
+
+    private Field operand;
+
     /** Constants used for return codes in Field.compare */
     public enum Op implements Serializable {
         EQUALS, GREATER_THAN, LESS_THAN, LESS_THAN_OR_EQ, GREATER_THAN_OR_EQ, LIKE, NOT_EQUALS;
@@ -58,7 +64,9 @@ public class Predicate implements Serializable {
      *            field value to compare passed in tuples to
      */
     public Predicate(int field, Op op, Field operand) {
-        // some code goes here
+        this.fieldNo = field;
+        this.op = op;
+        this.operand = operand;
     }
 
     /**
@@ -66,8 +74,7 @@ public class Predicate implements Serializable {
      */
     public int getField()
     {
-        // some code goes here
-        return -1;
+        return fieldNo;
     }
 
     /**
@@ -75,8 +82,7 @@ public class Predicate implements Serializable {
      */
     public Op getOp()
     {
-        // some code goes here
-        return null;
+        return op;
     }
     
     /**
@@ -84,8 +90,7 @@ public class Predicate implements Serializable {
      */
     public Field getOperand()
     {
-        // some code goes here
-        return null;
+        return operand;
     }
     
     /**
@@ -99,8 +104,7 @@ public class Predicate implements Serializable {
      * @return true if the comparison is true, false otherwise.
      */
     public boolean filter(Tuple t) {
-        // some code goes here
-        return false;
+        return t.getField(fieldNo).compare(op, operand);
     }
 
     /**
@@ -108,7 +112,6 @@ public class Predicate implements Serializable {
      * operand_string"
      */
     public String toString() {
-        // some code goes here
-        return "";
+        return "fieldNo:" + fieldNo + " operator:" + " operand:";
     }
 }
