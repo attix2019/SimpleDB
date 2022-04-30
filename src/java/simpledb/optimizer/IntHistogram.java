003d335a04e998e26451888400185cc8db81389d
chengr25 <384372602@qq.com>
2022-05-01 01:25:51 +0800

    lab3 通过了IntHistogramTest单元测试

--- src/java/simpledb/optimizer/IntHistogram.java
+++ src/java/simpledb/optimizer/IntHistogram.java
@@ -6,6 +6,22 @@ import simpledb.execution.Predicate;
  */
 public class IntHistogram {
 
+    private int bucketNum;
+
+    private int min;
+
+    private int max;
+
+    private int total;
+
+    // width = Math.ceil( tuplesNumber/ bucketNum)
+    private int width;
+
+    // needed if tuplesNumber % bucketNum != 0
+    private int lastBinWidth;
+
+    int[] bucketCount;
+
     /**
      * Create a new IntHistogram.
      * 
@@ -24,6 +40,13 @@ public class IntHistogram {
      */
     public IntHistogram(int buckets, int min, int max) {
     	// some code goes here
+        this.bucketNum = buckets;
+        this.min = min;
+        this.max = max;
+        this.total = 0;
+        this.width = (int)Math.ceil( (double)(max-min+1)/bucketNum );
+        lastBinWidth = (max-min+1) % bucketNum == 0 ? width:(max-min+1) % bucketNum;
+        bucketCount = new int[bucketNum];
     }
 
     /**
@@ -32,6 +55,10 @@ public class IntHistogram {
      */
     public void addValue(int v) {
     	// some code goes here
+        int dis = v - this.min;
+        int pos = dis / width;
+        total ++;
+        bucketCount[pos]++;
     }
 
     /**
@@ -45,9 +72,48 @@ public class IntHistogram {
      * @return Predicted selectivity of this particular operator and value
      */
     public double estimateSelectivity(Predicate.Op op, int v) {
+        double eqfrac = 0;
+        double lt = 0;
+        double gt = 0;
+        if(v < min){
+            lt  = 0;
+            gt = 1;
+        }else if(v > max){
+            lt = 1;
+            gt = 0;
+        }else{
+            int dis = v - min;
+            int pos = dis/width;
+            int w = (pos == bucketNum-1 && (max-min+1) % bucketNum != 0)? this.lastBinWidth: this.width;
+            eqfrac =  (double)1/w * bucketCount[pos]/total;
 
-    	// some code goes here
-        return -1.0;
+            for(int i = pos + 1; i < bucketNum; i++){
+                gt += ((double)bucketCount[i]/total);
+            }
+            int vLeft = min + width * pos;
+            int vRight = min + ((pos == bucketNum - 1 && lastBinWidth != width)? max :width *(pos + 1));
+            gt += (vRight - v - 1)*((double)bucketCount[pos]/total);
+
+            for(int i = 0; i < pos; i++){
+                lt += ((double)bucketCount[i]/total);
+            }
+            lt += (v - vLeft)*((double)bucketCount[pos]/total);
+        }
+
+
+        if(op == Predicate.Op.EQUALS){
+            return eqfrac;
+        }else if(op == Predicate.Op.GREATER_THAN){
+            return gt;
+        }else if(op == Predicate.Op.GREATER_THAN_OR_EQ){
+            return gt + eqfrac;
+        }else if(op == Predicate.Op.LESS_THAN){
+            return lt;
+        }else if(op == Predicate.Op.LESS_THAN_OR_EQ){
+            return lt + eqfrac;
+        }else{
+            return gt + lt;
+        }
     }
     
     /**
@@ -68,7 +134,6 @@ public class IntHistogram {
      * @return A string describing this histogram, for debugging purposes
      */
     public String toString() {
-        // some code goes here
-        return null;
+        return new String("bucktNumber:" + bucketNum);
     }
 }
